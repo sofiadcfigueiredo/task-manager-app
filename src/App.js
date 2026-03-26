1475fc08566d486d5907b8a88d8aac6d47c91ec7
sofiadcfigueiredo <sofiadcfigueiredo@outlook.com>
2026-03-26 15:47:23 +0000

    feat: add App component with navigation and global styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Tasks from './components/Tasks';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <nav className="navbar">
+          <div className="nav-container">
+            <h1 className="nav-title">Task Manager</h1>
+            <ul className="nav-menu">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/tasks">Tasks</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tasks" element={<Tasks />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
